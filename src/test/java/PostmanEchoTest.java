c57917f20641cecc3a10b1ed6bede47add3160a8
UralHawkies <hawkies@yandex.ru>
2022-09-12 20:35:22 +0500

    Fixed

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -15,7 +15,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some"))
+                .body("data", equalTo("something"))
         ;
     }
 }
